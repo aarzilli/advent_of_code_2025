ea8fd1d40c9d3abc989749a6e37b2d78760fd8bd
aarzilli <alessandro.arzilli@gmail.com>
2025-12-01 06:46:36 +0100

    day 01 cleanup

--- 01.go
+++ 01.go
@@ -12,7 +12,6 @@ func main() {
 	part1 := 0
 	part2 := 0
 	for _, instr := range lines {
-		Pln("moving", instr)
 		sign := 0
 		switch instr[0] {
 		case 'L':
@@ -22,44 +21,31 @@ func main() {
 		default:
 			panic("blah")
 		}
-		n := sign*Atoi(instr[1:])
+		n := sign * Atoi(instr[1:])
 		prevpos := pos
 		pos += n
 		pos %= 100
 		if pos < 0 {
 			pos += 100
 		}
-		Pln(pos)
 		if pos == 0 {
 			part1++
 		}
 		sign2 := 0
-		if pos - prevpos < 0 {
+		if pos-prevpos < 0 {
 			sign2 = -1
 		} else {
 			sign2 = +1
 		}
-		if sign2 != sign {
-			Pln("\tcrossed")
+		if sign2 != sign && prevpos != 0 {
+			part2++
+		} else if pos == 0 {
+			part2++
 		}
-		p2 := prevpos
-		for range Atoi(instr[1:]) {
-			p2 += sign
-			if p2 < 0 {
-				p2 = 99
-			}
-			if p2 == 100 {
-				p2 = 0
-			}
-			if p2 == 0 {
-				Pln("\tcrossed2")
-				part2++
-			}
-		}
-		if p2 != pos {
-			panic("mismatch")
-		}
+		part2 += Abs(n) / 100
 	}
+	Expect(992)
 	Sol(part1)
+	Expect(6133)
 	Sol(part2)
 }
