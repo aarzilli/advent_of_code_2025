5382c0aea45282f9fc94068334fbdc3ffa815424
aarzilli <alessandro.arzilli@gmail.com>
2025-12-03 07:08:31 +0100

    day 03 better

--- 03.go
+++ 03.go
@@ -11,9 +11,9 @@ func main() {
 	part2 := 0
 	for i := range lines {
 		v := Vatoi(Spac(lines[i], "", -1))
-		n := maxjoltage(2, v)
+		n := maxjoltage(pow10(2), v)
 		part1 += n
-		n = maxjoltage(12, v)
+		n = maxjoltage(pow10(12), v)
 		part2 += n
 	}
 	Sol(part1)
@@ -22,27 +22,32 @@ func main() {
 
 var curbest int
 
-func maxjoltage(rem int, v []int) int {
+func maxjoltage(base int, v []int) int {
 	curbest = 0
-	maxjoltageintl(0, rem, v)
+	maxjoltageintl(0, base, v)
 	return curbest
 }
 
-func maxjoltageintl(start, rem int, v []int) {
-	if rem == 0 {
-		if start > curbest {
-			curbest = start
-		}
-		return
-	}
-	base := 1
-	for range rem - 1 {
-		base *= 10
-	}
+func maxjoltageintl(start, base int, v []int) {
 	for i := range v {
 		if start+((v[i]+1)*base) < curbest {
 			continue
 		}
-		maxjoltageintl(start+(v[i]*base), rem-1, v[i+1:])
+		n := start + (v[i] * base)
+		if base == 1 {
+			if n > curbest {
+				curbest = n
+			}
+		} else {
+			maxjoltageintl(n, base/10, v[i+1:])
+		}
 	}
+}
+
+func pow10(n int) int {
+	x := 1
+	for range n - 1 {
+		x *= 10
+	}
+	return x
 }
