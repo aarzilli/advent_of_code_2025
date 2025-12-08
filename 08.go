f99ca7bd59807b9c17eb02fb180c33236441fb45
aarzilli <alessandro.arzilli@gmail.com>
2025-12-08 07:21:53 +0100

    day 08 cleanup

--- 08.go
+++ 08.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	. "aoc/util"
-	"os"
-	"math"
-	_ "sort"
-	"slices"
 	"cmp"
+	"math"
+	"os"
+	"slices"
 )
 
 type point struct {
@@ -14,130 +13,100 @@ type point struct {
 }
 
 var points []point
-var dist = map[int]map[int]float64{}
-var color []int
-var nextcolor = 1
+var conn = map[[2]int]bool{}
 
 type adist struct {
-	d float64
+	d    float64
 	i, j int
 }
 
 func main() {
 	lines := Input(os.Args[1], "\n", true)
-	Pf("len %d\n", len(lines))
-	
+
 	for _, line := range lines {
 		v := Vatoi(Spac(line, ",", -1))
-		points = append(points, point{ float64(v[0]), float64(v[1]), float64(v[2]) })
+		points = append(points, point{float64(v[0]), float64(v[1]), float64(v[2])})
 	}
-	
+
 	distv := []adist{}
-	
+
 	for i := range points {
-		for j := i+1; j < len(points); j++ {
-			d := math.Sqrt(math.Pow(points[i].x - points[j].x, 2) + math.Pow(points[i].y - points[j].y, 2) + math.Pow(points[i].z - points[j].z, 2))
-			if dist[i] == nil {
-				dist[i] = make(map[int]float64)
-			}
-			if dist[j] == nil {
-				dist[j] = make(map[int]float64)
-			}
-			dist[i][j] = d
-			dist[j][i] = d
-			
-			distv = append(distv, adist{ d, i, j })
+		for j := i + 1; j < len(points); j++ {
+			d := math.Sqrt(math.Pow(points[i].x-points[j].x, 2) + math.Pow(points[i].y-points[j].y, 2) + math.Pow(points[i].z-points[j].z, 2))
+			distv = append(distv, adist{d, i, j})
 		}
 	}
-	
-	slices.SortFunc(distv, func(a, b adist) int {
-		return cmp.Compare(a.d, b.d)
-	})
-	
-	Pln("distances done")
-	
-	color = make([]int, len(points))
-	
-	prevmin := 0.0
-	
-	/*N := 10
+
+	slices.SortFunc(distv, func(a, b adist) int { return cmp.Compare(a.d, b.d) })
+
+	N := 10
 	if len(lines) > 500 {
-		Pln("big input")
 		N = 1000
-	}*/
-	
+	}
+
+	comp0 := make(Set[int])
+	comp0.Add(0)
+
 	for cnt, ad := range distv {
-		//i, j := findmin(prevmin)
-		_ = prevmin
+		if cnt == N {
+			seen := make(Set[int])
+			countsv := []int{}
+			for len(seen) < len(points) {
+				for k := range points {
+					if !seen[k] {
+						sz := componentsz(k, seen)
+						if sz != 1 {
+							countsv = append(countsv, sz)
+						}
+					}
+				}
+			}
+			slices.Sort(countsv)
+			Pln(countsv)
+			Sol(countsv[len(countsv)-1] * countsv[len(countsv)-2] * countsv[len(countsv)-3])
+		}
+
 		i, j := ad.i, ad.j
-		Pln("Joining", points[i], points[j], cnt)
-		prevmin = dist[i][j]
+		//Pln("Joining", points[i], points[j], cnt)
 		if i == 0 && j == 0 {
 			break
 		}
-		c := 0
-		if color[i] != 0 && color[j] != 0 {
-			c = color[i]
-			c2 := color[j]
-			for k := range color {
-				if color[k] == c2 {
-					color[k] = c
-				}
-			}
-		} else if color[i] != 0 {
-			c = color[i]
-		} else if color[j] != 0 {
-			c = color[j]
-		} else {
-			c = nextcolor
-			nextcolor++
+
+		conn[[2]int{i, j}] = true
+		conn[[2]int{j, i}] = true
+
+		switch {
+		case comp0[i] && !comp0[j]:
+			componentsz(j, comp0)
+		case !comp0[i] && comp0[j]:
+			componentsz(i, comp0)
 		}
-		color[i] = c
-		color[j] = c
-		
-		allsame := true
-		c2 := color[0]
-		for i := range color {
-			if color[i] != c2 {
-				allsame = false
-				break
-			}
-		}
-		if allsame {
-			Pln("first connection is", points[i], points[j])
+
+		if len(comp0) == len(points) {
+			Pln("Final connection is", points[i], points[j])
 			Sol(int(points[i].x * points[j].x))
 			break
 		}
 	}
-	/*
-	Pln(color)
-	counts := map[int]int{}
-	for i := range color {
-		counts[color[i]]++
-	}
-	countsv := []int{}
-	for k, v := range counts {
-		if k == 0 {
-			continue
-		}
-		countsv = append(countsv, v)
-	}
-	Pln(counts)
-	sort.Ints(countsv)
-	Pln(countsv)
-	Sol(countsv[len(countsv)-1] * countsv[len(countsv)-2] * countsv[len(countsv)-3])*/
 }
 
-func findmin(prevmin float64) (int, int) {
-	minpair := [2]int{ 0, 0 }
-	first := true
-	for i := range points {
-		for j := i+1; j < len(points); j++ {
-			if dist[i][j] > prevmin && (dist[i][j] < dist[minpair[0]][minpair[1]] || first) {
-				first = false
-				minpair = [2]int{ i, j }
+func componentsz(k int, seen Set[int]) int {
+	comp := make(Set[int])
+	q := make(Set[int])
+	q.Add(k)
+	for len(q) > 0 {
+		cur := OneKey(q)
+		delete(q, cur)
+		if seen[cur] {
+			continue
+		}
+		seen.Add(cur)
+		comp.Add(cur)
+		for j := range points {
+			if conn[[2]int{cur, j}] && !seen[j] {
+				q.Add(j)
 			}
 		}
 	}
-	return minpair[0], minpair[1]
+	return len(comp)
 }
